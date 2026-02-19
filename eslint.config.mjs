97c6b97d9374b5bcf9032ed9211c6183f5afdc29
Smuks <60717893+TheSmuks@users.noreply.github.com>
2026-02-19 21:29:27 +0100

    chore: migrate from deprecated .eslintignore to eslint.config.mjs (closes #499) (#511)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,8 @@ export default [
     {
         ignores: [
             '**/dist/**',
+            '**/build/**',
+            '**/out/**',
             '**/*.tsbuildinfo',
             '**/coverage/**',
             '**/.nyc_output/**',
